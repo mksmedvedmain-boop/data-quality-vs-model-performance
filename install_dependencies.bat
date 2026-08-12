4fb1daf470931158c5209f358298049cc2564690
mksmedvedmain-boop <mks.medvedmain@gmail.com>
2026-08-12 17:18:26 +0300

    Update install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,1 +1,11 @@
+@echo off
 
+python -m venv .venv
+call .venv\Scripts\activate
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+echo.
+echo All dependencies were installed successfully.
+pause
